963608eb081ab033f363199c1c10f1472f85a819
vishnu jayakumar <vishnujayakumar1830@gmail.com>
2020-07-23 10:47:44 +0530

    Updates

---
+++ precache-manifest.fe663b55d5a567985747a0c8fb64369c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c3afbc70374fd3c5fdac1566f58fcb",
+    "url": "/landingpage-sample/index.html"
+  },
+  {
+    "revision": "5bd1a3709473cb375f4d",
+    "url": "/landingpage-sample/static/css/main.adeabaed.chunk.css"
+  },
+  {
+    "revision": "9921cdb706e85c8399e6",
+    "url": "/landingpage-sample/static/js/2.ea07f998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landingpage-sample/static/js/2.ea07f998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd1a3709473cb375f4d",
+    "url": "/landingpage-sample/static/js/main.17dcc3fd.chunk.js"
+  },
+  {
+    "revision": "15c4dc3f8d59bc8cbf76",
+    "url": "/landingpage-sample/static/js/runtime-main.10118e96.js"
+  }
+]);
